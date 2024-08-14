9f4b40096d434cf8c594b7ab0d59130379c02225
jDanek <jirka.danek@jdanek.eu>
2024-08-14 22:38:48 +0200

    Add test for $viaColumn parameter of Row::ref() method

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -371,6 +371,23 @@ class DatabaseTest extends TestCase
         $this->assertEquals('SELECT * FROM application WHERE (application.maintainer_id IN (11, 12))', $applications);
     }
 
+    public function testRefViaColumn()
+    {
+        $data = [];
+        foreach ($this->db->table('application') as $app) {
+            $author = $app->ref('author', 'author_id');
+            $maintainer = $app->ref('author', 'maintainer_id');
+            $data[] = $app['title'] . ': ' . $author['name'] . ' - ' . ($maintainer['name'] ?? 'N/A');
+        }
+
+        $this->assertEquals([
+            'Adminer: Jakub Vrana - Jakub Vrana',
+            'JUSH: Jakub Vrana - N/A',
+            'Nette: David Grudl - David Grudl',
+            'Dibi: David Grudl - David Grudl',
+        ], $data);
+    }
+
     public function testJoin()
     {
         $data = [];
